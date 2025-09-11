6980e6c25f354e81cc237c1ba6cc7b2a40aae218
MohammadiHadi <122750330+MohammadiHadi@users.noreply.github.com>
2025-09-11 15:36:46 -0400

    updated base path

--- fieldnote-react-1/vite.config.js
+++ fieldnote-react-1/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/COSI-153A-Workspace/fieldnote-react/', 
+  base: '/COSI-153A-Workspace/fieldnote-react-1/', 
 })
