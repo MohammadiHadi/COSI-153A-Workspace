a05fe52e3a0d8bd6828a15ccb08a61320b382e17
MohammadiHadi <122750330+MohammadiHadi@users.noreply.github.com>
2025-10-29 14:34:45 -0400

    Added new routes to the Express server

--- simple-server/index.js
+++ simple-server/index.js
@@ -3,9 +3,11 @@ import express from 'express';
 
 // Create an Express application instance
 const app = express();
+app.use(express.json()); 					// enable JSON body parsing
+app.use(express.urlencoded({ extended: true }));	// enable form body parsing
 
-// Define the server port number
-const PORT = 3000;
+// // Define the server port number
+// const PORT = 3000;
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
@@ -13,5 +15,41 @@ app.get('/', (req, res) => {
   res.send("Backend server is live!");
 });
 
+app.get('/status', (req, res) => {
+  // Send a simple text response to the client
+  res.send("This is the status page");
+});
+
+app.get('/about', (req, res) => {
+  res.send("This is the about page");
+});
+
+app.post("/register", (req, res) => {
+  res.send("Form for user registration submitted successfully");
+});
+
+app.get('/user/:id', (req,res)=>{
+ const userId = req.params.id; // access the parameter value
+ res.send(`User ID is: ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+  const course = req.query.course;
+  const id = req.query.id;
+  res.send(`Search for ${id}: ${course}`);
+});
+
+app.post('/register', (req, res) => {
+  console.log(req.body); // parsed data
+  res.send(`Hello, ${req.body.name}!`);
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.body); // parsed data
+  res.send(`Hello, ${req.body.name}!`);
+});
+
+
+
 // Start the server and listen on the defined port
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(3000, () => console.log(`Server running on http://localhost:3000`));
