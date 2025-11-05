7179f64c41120327c98d662d61d0225e91e7de87
MohammadiHadi <122750330+MohammadiHadi@users.noreply.github.com>
2025-11-05 13:24:44 -0500

    Database schema and model for Notes

--- fieldnote-server/controllers/notesCtroller.js
+++ fieldnote-server/controllers/notesCtroller.js
@@ -1,14 +1,30 @@
-const notes = [
-    { title: 'Hello', body: 'This is my first note', createdAt: Date.now()}
-  ]
+import { Note } from '../models/notes.js';
 
-export const getNotes = (req, res) => {
-  res.json(notes);
-}
 
-export const addNote = (req, res) => {
-  const { title, body } = req.body;
-  const newNote = { title, body, createdAt: Date.now() };
-  notes.push(newNote);
+// export const getNotes = (req, res) => {
+//   res.json(notes);
+// }
+
+// export const addNote = (req, res) => {
+//   const { title, body } = req.body;
+//   const newNote = { title, body, createdAt: Date.now() };
+//   notes.push(newNote);
+//   res.json(notes);
+// }
+
+export const getNotes = async (req, res) => {
+  const notes = await Note.find();
   res.json(notes);
-}
+};
+
+
+
+export const addNote = async (req, res) => {
+  try {
+    const note = new Note(req.body);
+    await note.save();
+    res.status(201).json(note);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
